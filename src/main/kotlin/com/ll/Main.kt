748e8df1119bc491201115faf7364822b9015a4d
Gooraeng <birdyoon1998@gmail.com>
2025-08-11 15:43:06 +0900

    done step 2

--- src/main/kotlin/com/ll/Main.kt
+++ src/main/kotlin/com/ll/Main.kt
@@ -6,10 +6,19 @@ fun main() {
     while (true) {
         print("명령) ")
 
-        val command = readlnOrNull()
+        val command = tryInput()
 
-        if (command == "종료") {
-            break
+        if (command == "종료") break
+
+        if (command == "등록") {
+            print("명언) ")
+            val content = tryInput()
+            print("작가) ")
+            val author = tryInput()
         }
     }
+}
+
+fun tryInput() : String{
+    return readlnOrNull()!!.trim()
 }
